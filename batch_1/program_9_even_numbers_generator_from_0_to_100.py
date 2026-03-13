91a1ee057d3b0b0a5265f30106b7c4bf7c19f270
unknown <achillesaeon8-svg>
2026-03-13 23:22:43 +0800

    Done batch 1 program 9

--- batch_1/program_9_even_numbers_generator_from_0_to_100.py
+++ batch_1/program_9_even_numbers_generator_from_0_to_100.py
@@ -1,5 +1,13 @@
-first_number = int(input("Enter a number: "))
+users_decision = input("Do you want generate even numbers only from 0 to 100? Yes or No: ")
 
-result = first_number % 2
+if users_decision.lower() == "yes":
+        print("These are all the even numbers only from 0 to 100:", end=" ")
 
-print("The resulted number is", result)
+        for i in range(2, 101, 2):
+            print(i, end=" ")
+
+elif users_decision.lower() == "no":
+    print("Thank you for considering our generator.")
+
+else:
+    print("Invalid input. Please try again.")
