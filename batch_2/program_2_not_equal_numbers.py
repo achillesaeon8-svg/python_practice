00ba0d96d41547c45a4461d36183b0b10867c8c6
unknown <achillesaeon8-svg>
2026-03-21 00:10:55 +0800

    Batch 2 prog 2 done

---
+++ batch_2/program_2_not_equal_numbers.py
@@ -0,0 +1,9 @@
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
+
+if first_number == second_number:
+    print("Both numbers are equal")
+elif first_number != second_number:
+    print("Both numbers are not equal")
+else:
+    print("incorrect input")
