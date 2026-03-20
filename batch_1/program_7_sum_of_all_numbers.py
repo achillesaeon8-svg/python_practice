a68edf82adfd0fa6e62d3d3b03fbe377eb079b2e
unknown <achillesaeon8-svg>
2026-03-21 00:05:40 +0800

    Batch 2 Update

--- batch_1/program_7_sum_of_all_numbers.py
+++ batch_1/program_7_sum_of_all_numbers.py
@@ -2,8 +2,12 @@ chosen_numbers = []
 print("Enter 10 numbers: ")
 
 for i in range(9):
-    numbers = float(input("Enter {i+1} numbers: "))
-    chosen_numbers.append(numbers)
-    all_numbers_sum = chosen_numbers
-
+    while True:
+        try:
+            num = float(input("Enter number" {i+1}": "))
+            chosen_numbers.append(numbers)
+            break
+        except ValueError:
+            print("Invalid Input.")
+                
 print("The sum of all 10 numbers are", all_numbers_sum)
