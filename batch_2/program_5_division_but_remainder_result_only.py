1aa7a164f09a5abf0a7ee44c07ff2fff5fe6e711
unknown <achillesaeon8-svg>
2026-03-24 23:21:44 +0800

    Batch 2 program 5 done, bathc 2 program 4 done* correction for past commit

---
+++ batch_2/program_5_division_but_remainder_result_only.py
@@ -0,0 +1,6 @@
+first_number = int(input('Enter a first number: '))
+second_number = int(input('Enter a second number: '))
+
+remainder = second_number % first_number
+
+print(f'{remainder} is the remainder of two numbers by dividing the first number to the second number.')
