1cae0db72559698d9f24f555389881fe7f0fb9b6
unknown <achillesaeon8-svg>
2026-04-03 17:08:13 +0800

    Batch 2 program 6 Done

--- batch_2/program_6_result_of_the_1st_number_subtracting_from_the_remaining_numbers.py
+++ batch_2/program_6_result_of_the_1st_number_subtracting_from_the_remaining_numbers.py
@@ -1,8 +1,20 @@
-# for i in range(10):
-#     numbers = int(input(f'Enter a number {i+1}: '))
-#     numbers.append()
+selected_numbers = 0
 
-# for numbers in i [1:10]:
-#     result -= numbers
+print('Enter 10 numbers: ')
 
-# print(f'{result} is the result of subtracting the first number to the remaining numbers.')
+for numerical_range in range(1, 11):
+    while True:
+        try:
+                
+            inputted_numbers = int(input(f'Enter number {numerical_range}: '))
+            if numerical_range == 1:
+                selected_numbers = inputted_numbers
+            else:
+                selected_numbers -= inputted_numbers
+
+            break
+            
+        except ValueError:
+            print('Invalid Input. Please try again.')            
+
+print(f'{selected_numbers} is the result of subtracting the first number you had inputted to the remaining numbers.')
