bd7532d444eb84ae949d5ae9d4960ecada27517d
unknown <achillesaeon8-svg>
2026-04-03 21:51:59 +0800

    batch 2 program 9 done

--- batch_2/program_9_no_zero_or_five_ending_numbers_generator_from_0_to_100.py
+++ batch_2/program_9_no_zero_or_five_ending_numbers_generator_from_0_to_100.py
@@ -1,9 +1,11 @@
-users_decision = input('Do you want to distinguish all numbers starting from 0 to 100 except numbers ending 0? Yes or No: ')
+users_decision = input('Do you want to distinguish all numbers starting from 0 to 100 except numbers ending 0 or 5? Yes or No: ')
 
 if users_decision.lower() == 'yes':
     for number_sequence in range(101):
         
         if str(number_sequence).endswith('0'):
+            continue
+        elif str(number_sequence).endswith('5'):
             continue
         if number_sequence == 99:
             print(number_sequence, end='.')
