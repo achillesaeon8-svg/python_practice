83d075f91f9be0ae4a276ac6ff0bdeb53b4b0352
unknown <achillesaeon8-svg>
2026-04-05 03:24:21 +0800

    batch 4 program 1 done

---
+++ batch_4/program_1_duplicated_numbers_indenticator.py
@@ -0,0 +1,24 @@
+stored_numbers = []
+duplicated_numbers = []
+
+print('Enter 10 numbers: ')
+
+for numerical_range in range(10): 
+
+    inputted_numbers = int(input(f'Number {numerical_range+1}: '))
+
+    if inputted_numbers in stored_numbers and inputted_numbers not in duplicated_numbers:
+        duplicated_numbers.append(inputted_numbers)
+
+    stored_numbers.append(inputted_numbers)
+
+duplicated_range = len(duplicated_numbers)
+list.sort(duplicated_numbers)
+duplicated_strings = ', '.join(map(str, duplicated_numbers))
+
+if duplicated_range == 1:
+    print(f'The duplicated numbers is {duplicated_strings}.')
+elif duplicated_range >= 2:
+    print(f'The duplicated numbers are {duplicated_strings}.')
+else:
+    print(f'There are no duplicated.')
