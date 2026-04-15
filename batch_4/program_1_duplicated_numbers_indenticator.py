9ba94eb751b36076b3b9ee1d17273888c2b4ef27
unknown <achillesaeon8-svg>
2026-04-15 23:44:32 +0800

    Batch 4 Instructions Added in each Program

--- batch_4/program_1_duplicated_numbers_indenticator.py
+++ batch_4/program_1_duplicated_numbers_indenticator.py
@@ -1,5 +1,7 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
 stored_numbers = []
-duplicated_numbers = []
+no_duplicated_numbers = []
 
 print('Enter 10 numbers: ')
 
@@ -7,15 +9,18 @@ for numerical_range in range(10):
 
     inputted_numbers = int(input(f'Number {numerical_range+1}: '))
 
+    if inputted_numbers in stored_numbers and inputted_numbers not in no_duplicated_numbers:
+        no_duplicated_numbers.append(inputted_numbers)
+
     stored_numbers.append(inputted_numbers)
 
-duplicated_range = len(duplicated_numbers)
-list.sort(duplicated_numbers)
-duplicated_strings = ', '.join(map(str, duplicated_numbers))
+all_numbers_range = len(no_duplicated_numbers)
+list.sort(no_duplicated_numbers)
+all_numbers_strings = ', '.join(map(str, no_duplicated_numbers))
 
-if duplicated_range == 1:
-    print(f'The duplicated numbers is {duplicated_strings}.')
-elif duplicated_range >= 2:
-    print(f'The duplicated numbers are {duplicated_strings}.')
+if all_numbers_range == 1:
+    print(f'The duplicated numbers is {all_numbers_strings}.')
+elif all_numbers_range >= 2:
+    print(f'The duplicated numbers are {all_numbers_strings}.')
 else:
     print(f'There are no duplicated.')
