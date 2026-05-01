181286350095e14e5416fd2ebcb542647afaed3f
unknown <achillesaeon8-svg>
2026-05-01 22:19:55 +0800

    Batch 3 Program 4 foundation done

--- batch_3/program_4_lowest_number_identicator_upon_invalid_entry.py
+++ batch_3/program_4_lowest_number_identicator_upon_invalid_entry.py
@@ -1,1 +1,13 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number.
+
+stored_numbers = []
+
+while True:
+    inputted_characters = input('Enter a number: ')
+
+    try:
+        inputted_numbers = int(inputted_characters)
+
+    except ValueError:
+        print('You had not input a number.')
+        break   
