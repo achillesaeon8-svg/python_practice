645c3165b8b9d4c30546440227be8d67a0ab450b
unknown <achillesaeon8-svg>
2026-05-01 16:40:49 +0800

    Batch 3 Program 3 Foundation Done

--- batch_3/program_3_distinct_and_duplicated_numbers_identicator_upon_invalid_entry.py
+++ batch_3/program_3_distinct_and_duplicated_numbers_identicator_upon_invalid_entry.py
@@ -1,1 +1,15 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+while True:
+    try:
+        inputted_numbers = int(input('Enter a number: '))
+        stored_numbers = []
+        stored_numbers = inputted_numbers
+        
+        if inputted_numbers != stored_numbers:
+            print('Unique')
+        else:
+            print('Duplicate')
+    except:
+        print('You had not input a number.')
+        break
