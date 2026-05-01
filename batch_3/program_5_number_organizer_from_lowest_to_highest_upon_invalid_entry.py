1e7047c3bdc3d7e9802053528bf8529d44c97868
unknown <achillesaeon8-svg>
2026-05-01 22:58:16 +0800

    Batch 3 Program 5 Foundation Done

--- batch_3/program_5_number_organizer_from_lowest_to_highest_upon_invalid_entry.py
+++ batch_3/program_5_number_organizer_from_lowest_to_highest_upon_invalid_entry.py
@@ -1,1 +1,11 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function.
+
+while True:
+    inputted_characters = input('Enter a number: ')
+
+    try:
+        inputted_numbers = int(inputted_characters)
+
+
+    except:
+        print('You have not input a number.')
